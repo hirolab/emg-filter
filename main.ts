5d0cdcf323768153f13db90801a35654f35de62e
lifanwu <keystoradio@gmail.com>
2022-09-01 16:29:25 -0400

    set the pin 3

--- main.ts
+++ main.ts
@@ -1,3 +1,35 @@
-basic.forever(function () {
-	
-})
+let ave = 0
+let sum = 0
+let list: number[] = []
+let EMG = 0
+for (let index = 0; index <= 15; index++) {
+    EMG = pins.analogReadPin(AnalogPin.P3)
+    list.push(EMG)
+}
+
+
+namespace EmgFilter {
+    /**
+     * Use dynamic average window to filter EMG signal 
+     */
+    //% blockId = EmgFilter
+    //% block="set P3 pin as analog read pin"
+    //% inlineInputMode=inline
+    export function EmgFilter(): void {
+        EMG = pins.analogReadPin(AnalogPin.P3)
+        list.unshift(EMG)
+        list.removeAt(15)
+        sum = 0
+        for (let index = 0; index <= 15; index++) {
+            sum = sum + list[index]
+        }
+        ave = sum / 15
+        led.plotBarGraph(
+            ave,
+            1023
+        )
+    }
+}
+
+
+
