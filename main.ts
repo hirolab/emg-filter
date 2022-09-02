4e548ceca8940907184a3dc5b28dafb74542ac32
lifanwu <keystoradio@gmail.com>
2022-09-02 15:21:39 -0400

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -12,6 +12,11 @@ for (let index = 0; index <= 15; index++) {
     list.push(EMG)
 }
 
+let angle = 60
+let max_angle = 160
+let min_angle = 20
+servos.P0.setAngle(angle)
+
 namespace EmgFilter {
     /**
      * Use dynamic average window to filter EMG signal 
@@ -29,6 +34,31 @@ namespace EmgFilter {
         ave = sum / 15
         return ave
     }
+
+
+    /**
+     * Address motor rotation direction and speed by acceleration
+     */
+    //% blockId = servoturnspeed
+    //% block="set P0 servo rotates in the direction %direction by %speed (ms) interval"
+    export function SetTurnSpeed(direction: number, speed: number): void {
+        if (Math.abs(direction) >= 100) {
+            if (direction <= 0) {
+                angle = angle - 3
+            } else {
+                angle = angle + 3
+            }
+            if (angle <= min_angle) {
+                angle = min_angle
+            }
+            if (angle >= max_angle) {
+                angle = max_angle
+            }
+            servos.P0.setAngle(angle)
+        }
+        basic.pause(speed)
+    }
+
 }
 
 
