755069973683ec3b5f586a389436d98c360388c1
lifanwu <keystoradio@gmail.com>
2022-09-02 15:24:56 -0400

    Restore '9/2/2022, 1:24:27 PM Customize block'

--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
+
 /**
  * EMG blocks
  */
-//% weight=80 color=#0fbc11 icon=""
+//% weight = 80 color=#0fbc11 icon=""
 
 let ave = 0
 let sum = 0
@@ -12,10 +13,6 @@ for (let index = 0; index <= 15; index++) {
     list.push(EMG)
 }
 
-let angle = 60
-let max_angle = 160
-let min_angle = 20
-servos.P0.setAngle(angle)
 
 namespace EmgFilter {
     /**
@@ -34,31 +31,6 @@ namespace EmgFilter {
         ave = sum / 15
         return ave
     }
-
-
-    /**
-     * Address motor rotation direction and speed by acceleration
-     */
-    //% blockId = servoturnspeed
-    //% block="set P0 servo rotates in the direction %direction by %speed (ms) interval"
-    export function SetTurnSpeed(direction: number, speed: number): void {
-        if (Math.abs(direction) >= 100) {
-            if (direction <= 0) {
-                angle = angle - 3
-            } else {
-                angle = angle + 3
-            }
-            if (angle <= min_angle) {
-                angle = min_angle
-            }
-            if (angle >= max_angle) {
-                angle = max_angle
-            }
-            servos.P0.setAngle(angle)
-        }
-        basic.pause(speed)
-    }
-
 }
 
 
